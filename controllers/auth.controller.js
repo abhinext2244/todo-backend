17bb0157b11cbb69b7bdeb2a03592ecbbec04046
Abhishek kumar <115467885+abhinext2244@users.noreply.github.com>
2025-12-30 22:03:28 +0530

    Update cookie settings for access token

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -62,8 +62,8 @@ export const login = async (req, res) => {
 
     res.cookie("access_token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "development",
-      sameSite: "lax",
+      secure: true,
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000,
     });
 
